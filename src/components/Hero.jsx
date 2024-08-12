5e5a3a64c749812c9fd90ea8a326c715b0e43013
Abdullah Al Noman <154955210+AlNomanCSE@users.noreply.github.com>
2024-08-12 15:32:33 +0600

    herosection animation added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,62 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { HERO_CONTENT } from "../data.js";
 import profileImage from "../assets/Profile.jpg";
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 const Hero = () => {
   return (
     <section className="border-b border-neutral-900 pb-4 lg:mb-36">
       <div className="flex flex-wrap items-center ">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16"
+            >
               Fatema Tuj Johora Korobi
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+            >
               Data Science and Machine Learning Enthusiast
-            </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg-p-8">
           <div className="flex justify-center">
-            <div className="relative inline-block">
+            <motion.div
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              className="relative inline-block"
+            >
               <img
                 src={profileImage}
                 alt=""
                 className="rounded-lg brightness-75 w-[400px] object-cover"
               />
               <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent rounded-lg"></div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
